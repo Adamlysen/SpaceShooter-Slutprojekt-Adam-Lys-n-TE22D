a13410244b1e7dff613788d0c553d4023ee14ee7
Adam Lysén <adam.lysen@elev.ga.ntig.se>
2024-03-21 10:59:23 +0100

    vfdvdfvfd

--- SpaceShooter Slutprojekt Adam Lysén TE22D/Program.cs
+++ SpaceShooter Slutprojekt Adam Lysén TE22D/Program.cs
@@ -1,30 +1,35 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 using Raylib_cs;
 
-class Program
-{
-    static void Main()
-    {
-        Raylib.SetTargetFPS(60);
-        Raylib.InitWindow(600, 900, "SpaceShooter");
 
-        while (!Raylib.WindowShouldClose())
-        {
-            Raylib.BeginDrawing();
-            StartScreen();
-            Raylib.ClearBackground(Color.Blue);
-            Raylib.DrawText("GAY", 200, 200, 50, Color.Black);
-            Raylib.EndDrawing();
-        }
+Raylib.SetTargetFPS(60);
+Raylib.InitWindow(600, 900, "SpaceShooter");
+
+Texture2D bgtexture = Raylib.LoadTexture(@"spacebg.gif");
+bool Scenegame1 = false;
+
+while (!Raylib.WindowShouldClose())
+{
+    StartScreen();
+    Scenegame1 = true;
+    while (Scenegame1)
+    {
+        Raylib.BeginDrawing();
+        Raylib.DrawTexture(bgtexture, 0, 0, Color.White);
+        Raylib.EndDrawing();
     }
 
-    static void StartScreen()
+}
+
+static void StartScreen()
+{
+    while (!Raylib.IsKeyPressed(KeyboardKey.Enter))
     {
-        while (!Raylib.IsKeyPressed(KeyboardKey.Enter))
-        {
-            Raylib.ClearBackground(Color.White);
-            Raylib.DrawText("Press Enter to Start", 50, 250, 45, Color.Black);
-        }
-       
+        Raylib.BeginDrawing();
+        Raylib.ClearBackground(Color.White);
+        Raylib.DrawText("Press Enter to Start", 50, 250, 45, Color.Black);
+        Raylib.EndDrawing();
     }
 }
+
