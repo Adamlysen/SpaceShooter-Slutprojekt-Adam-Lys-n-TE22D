0f46f408c2a2df8964afbcd21470122aa7570a05
Adam Lysén <adam.lysen@elev.ga.ntig.se>
2024-03-21 10:23:47 +0100

    ijijikok

---
+++ SpaceShooter Slutprojekt Adam Lysén TE22D/Player.cs
@@ -0,0 +1,11 @@
+using System;
+using Raylib_cs;
+
+public class Player
+{
+    public int x = 10;
+    static void Update()
+    {
+
+    }
+}
---
+++ SpaceShooter Slutprojekt Adam Lysén TE22D/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Raylib_cs;
+
+class Program
+{
+    static void Main()
+    {
+        Raylib.SetTargetFPS(60);
+        Raylib.InitWindow(600, 900, "SpaceShooter");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            StartScreen();
+            Raylib.ClearBackground(Color.Blue);
+            Raylib.DrawText("GAY", 200, 200, 50, Color.Black);
+            Raylib.EndDrawing();
+        }
+    }
+
+    static void StartScreen()
+    {
+        while (!Raylib.IsKeyPressed(KeyboardKey.Enter))
+        {
+            Raylib.ClearBackground(Color.White);
+            Raylib.DrawText("Press Enter to Start", 50, 250, 45, Color.Black);
+        }
+       
+    }
+}
