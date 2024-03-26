c6ac538dcbe70867afc877b7a9ed4c2ee43e8709
Adam Lysén <adam.lysen@elev.ga.ntig.se>
2024-03-26 10:52:16 +0100

    sadasdasfsdf

--- SpaceShooter Slutprojekt Adam Lysén TE22D/Program.cs
+++ SpaceShooter Slutprojekt Adam Lysén TE22D/Program.cs
@@ -4,32 +4,39 @@ using Raylib_cs;
 
 
 Raylib.SetTargetFPS(60);
-Raylib.InitWindow(600, 900, "SpaceShooter");
+Raylib.InitWindow(506, 900, "SpaceShooter");
 
-Texture2D bgtexture = Raylib.LoadTexture(@"spacebg.gif");
-bool Scenegame1 = false;
+Texture2D bgtexture = Raylib.LoadTexture(@"spacebg4.png");
+Texture2D PlayerTexture = Raylib.LoadTexture(@"Spaceship2.png");
+bool Game = false;
+bool Start = true;
 
 while (!Raylib.WindowShouldClose())
 {
-    StartScreen();
-    Scenegame1 = true;
-    while (Scenegame1)
-    {
-        Raylib.BeginDrawing();
-        Raylib.DrawTexture(bgtexture, 0, 0, Color.White);
-        Raylib.EndDrawing();
-    }
-
-}
-
-static void StartScreen()
-{
-    while (!Raylib.IsKeyPressed(KeyboardKey.Enter))
+    if (Start)
     {
         Raylib.BeginDrawing();
         Raylib.ClearBackground(Color.White);
-        Raylib.DrawText("Press Enter to Start", 50, 250, 45, Color.Black);
+        Raylib.DrawText("Press Enter to Start", 25, 250, 40, Color.Black);
+        Raylib.EndDrawing();
+
+        if (Raylib.IsKeyPressed(KeyboardKey.Enter)) {
+            Start = false;
+            Game = true;
+        }
+    }
+    else if (Game)
+    {
+        Raylib.BeginDrawing();
+        Raylib.DrawTexture(bgtexture, 0, 0, Color.White);
+        Raylib.DrawTexture(PlayerTexture, 205, 600, Color.White);
         Raylib.EndDrawing();
     }
+
+    else if (!Game && !Start)
+    {
+
+    }
+
 }
 
--- SpaceShooter Slutprojekt Adam Lysén TE22D/obj/Debug/net8.0/SpaceShooter Slutprojekt Adam Lysén TE22D.AssemblyInfo.cs
+++ SpaceShooter Slutprojekt Adam Lysén TE22D/obj/Debug/net8.0/SpaceShooter Slutprojekt Adam Lysén TE22D.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpaceShooter Slutprojekt Adam Lysén TE22D")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f46f408c2a2df8964afbcd21470122aa7570a05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a13410244b1e7dff613788d0c553d4023ee14ee7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpaceShooter Slutprojekt Adam Lysén TE22D")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpaceShooter Slutprojekt Adam Lysén TE22D")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
