025f4094c73046adc3e0a7604038c013b471d710
Adam Lysén <adam.lysen@elev.ga.ntig.se>
2024-04-19 11:26:34 +0200

    Grundläggande spel klart

--- SpaceShooter Slutprojekt Adam Lysén TE22D/obj/Debug/net8.0/SpaceShooter Slutprojekt Adam Lysén TE22D.AssemblyInfo.cs
+++ SpaceShooter Slutprojekt Adam Lysén TE22D/obj/Debug/net8.0/SpaceShooter Slutprojekt Adam Lysén TE22D.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpaceShooter Slutprojekt Adam Lysén TE22D")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+963ff78932dbb97b0afb394e62b9e9ba1b700adc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7edb64611c3d7680b43379cafc6768eec79c8cdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpaceShooter Slutprojekt Adam Lysén TE22D")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpaceShooter Slutprojekt Adam Lysén TE22D")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
